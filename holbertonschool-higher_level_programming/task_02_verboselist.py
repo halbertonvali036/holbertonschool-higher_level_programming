1e3904b4f3c34205a6323a248fc38dd3f9221312
valii36 <valiiv-azmpa101@code.edu.az>
2026-06-23 23:44:58 +0400

    PythonTask

---
+++ holbertonschool-higher_level_programming/task_02_verboselist.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, x):
+        item_count = len(x)
+        super().extend(x)
+        print(f"Extended the list with [{item_count}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
