e445bdb08a4628a18e64f0ca1648d8a6ab264e2b
valii36 <valiiv-azmpa101@code.edu.az>
2026-06-25 23:30:52 +0400

    dsfa

--- python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-for code in range(97, 123):
-    print("{}".format(chr(code)), end="")
-    
--- python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-for code in range(97, 123):
-    if code != 101 and code != 113:
-        print("{}".format(chr(code)), end="")
-        
