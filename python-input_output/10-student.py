9eb64af2d78448ab716faa22813cfbf6702b15bf
valii36 <valiiv-azmpa101@code.edu.az>
2026-06-26 20:49:34 +0400

    sayxgagfafffvak

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Module that defines a Student class."""
+"""Module that defines a Student class"""
 
 
 class Student:
-    """Student class."""
+    """Defines a student by first_name, last_name and age"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Return the dictionary description of the instance."""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__.copy()
