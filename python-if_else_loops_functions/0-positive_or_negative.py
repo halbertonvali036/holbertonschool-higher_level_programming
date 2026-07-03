d07371b26623a4dec6bb46ddf539346637f8e3ac
valii36 <valiiv-azmpa101@code.edu.az>
2026-07-04 00:07:44 +0400

    adfsaq

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,8 +4,8 @@ import random
 number = random.randint(-10, 10)
 
 if number > 0:
-    print(f"{number} is positive")
+    print(f"{number} musbetdir")
 elif number == 0:
-    print(f"{number} is zero")
+    print(f"{number} sifirdir")
 else:
-    print(f"{number} is negative")
+    print(f"{number} menfidir")
