3eec21cf03a6d623194dbbbc401cb3992e529b00
valii36 <valiiv-azmpa101@code.edu.az>
2026-07-04 02:08:44 +0400

    SDAFqkpio

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
+    print("{:02d}".format(i), end="")
     if i < 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+        print(", ", end="")
+    if (i + 1) % 18 == 0:
+        print("\n", end="")
+print()
