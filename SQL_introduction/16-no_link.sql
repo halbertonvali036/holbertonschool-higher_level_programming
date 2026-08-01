b2376ccb63f2a6e8ffd9304128e52a02bddf906f
valii36 <valiiv-azmpa101@code.edu.az>
2026-08-01 20:03:48 +0400

    Rklkkkkhgjh

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Lists all records of second_table having a valid name ordered by score (descending)
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
