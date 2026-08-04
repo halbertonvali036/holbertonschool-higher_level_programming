17ac0ee94ac1c44dd70c1e60fc88273b609e4e61
valii36 <valiiv-azmpa101@code.edu.az>
2026-08-04 18:20:48 +0400

    dafd

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const count = process.argv.length - 2;
 
-if (count === 0) {
+const args = process.argv.length - 2;
+
+if (args === 0) {
   console.log('No argument');
-} else if (count === 1) {
+} else if (args === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
