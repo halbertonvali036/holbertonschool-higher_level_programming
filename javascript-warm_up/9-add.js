258d0a56b95f233c211de4e8732c75b58e29c985
valii36 <valiiv-azmpa101@code.edu.az>
2026-08-05 15:34:26 +0400

    dfad

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+const firstArg = parseInt(process.argv[2], 10);
+const secondArg = process.argv[3];
+const secondNum = parseInt(secondArg, 10);
 
-console.log(add(a, b));
+if (process.argv.length < 4 && secondArg === undefined) {
+  console.log(NaN);
+} else {
+  console.log(add(firstArg, secondNum));
+}
